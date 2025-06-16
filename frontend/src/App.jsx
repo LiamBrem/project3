d4807dc683e85ae6b9aaf2bb197c20fc074a10a3
Liam Edward Brem <liambrem@meta.com>
2025-06-16 15:04:02 -0700

    basic header/footer layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Search from "./components/outer/Search";
+import Sort from "./components/outer/Sort";
 
 function App() {
-
   return (
     <>
+      <banner className="banner">
+        <div className="title-container">
+          <h1>Kudos Board</h1>
+        </div>
+
+        <div className="controls">
+          <Search />
+          <Sort />
+        </div>
+        <div className="rain"></div>
+      </banner>
+      <section className="content">
+        
+      </section>
+      <footer className="footer">
+        <h3>By Liam Brem</h3>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,7 @@
+export const SORT_OPTIONS = {
+  ALL: "All",
+  RECENT: "Recent",
+  CELEBRAION: "Celebration",
+  THANK_YOU: "Thank You",
+  INSPIRATION: "Inspiration",
+};
